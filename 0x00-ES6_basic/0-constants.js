ba7a1d498e0024841f8d066bbd612378b966fa53
Okibe Onmeje <Okibeogomola@gmail.com>
2024-06-25 00:26:52 +0100

    fix declaration
    changed variable declarations with var to const and let

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,19 @@
+function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+function getLast() {
+  return ' is okay';
+}
+
+function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+  return combination;
+}
+module.exports = {
+  taskNext,
+  getLast,
+  taskFirst,
+};
---
+++ 0x00-ES6_basic/README.md
@@ -0,0 +1,3 @@
+## Scripts in this directory illustrates basic ES6 concepts.
+### 0-constants.js
+	Illustrates the use of and const.
