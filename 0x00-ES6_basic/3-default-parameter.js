3fee6f6329fc8a0309d51338207c1f9a001c5f1b
Okibe Onmeje <Okibeogomola@gmail.com>
2024-06-29 13:56:16 +0100

    fix eslint error function param reassignment

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,12 +1,5 @@
-function getSumOfHoods(initialNumber = undefined,
-  expansion1989 = undefined, expansion2019 = undefined) {
-  if (expansion1989 === undefined) {
-    expansion1989 = 89;
-  }
-
-  if (expansion2019 === undefined) {
-    expansion2019 = 19;
-  }
+function getSumOfHoods(initialNumber = 0,
+  expansion1989 = 89, expansion2019 = 19) {
   return initialNumber + expansion1989 + expansion2019;
 }
 
