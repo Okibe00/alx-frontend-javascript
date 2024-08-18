f2c287f3305eb55d395978ee36e3ba2a2d41aa5b
Okibe Onmeje <Okibeogomola@gmail.com>
2024-08-18 10:50:05 +0100

    fix unused variable

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then((success) => {
+  return promise.then(() => {
     console.log('Got a response from the API');
     return {
       status: 200,
